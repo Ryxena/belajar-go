f45b8bbffedc16266ccea9081d0608ad27b21450
Ryxena <rubbenme@proton.me>
2024-03-16 20:07:20 +0700

    Variable in go

---
+++ variable/var.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "Rubben"
+	var (
+		halo = "halo"
+		dunia = "world"
+	)
+	github := "Ryxena"
+	fmt.Println(name)
+	fmt.Println(halo)
+	fmt.Println(dunia)
+	fmt.Println(github)
+}
